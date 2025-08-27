42f54c1b3eecd40353d7e114418e813da7e084cb
TeepakornPC <teeprakorn.toey@gmail.com>
2025-08-28 02:57:19 +0700

    VERSION 0.3

--- Jwt_Tokens/Tokens_Generator.js
+++ Jwt_Tokens/Tokens_Generator.js
@@ -2,27 +2,23 @@ const e = require('cors');
 const jwt = require('jsonwebtoken');
 
 // Generator Token
-function Tokens_Generator(Users_ID, Users_Email, Users_Username, UsersType_ID, Users_Type, Login_Type) {
-  if (!Users_ID || !Users_Email || !Users_Username || !UsersType_ID || !Users_Type || !Login_Type) {
+function Tokens_Generator(Users_ID, Users_Email, Users_Username, UsersType_ID, Users_Type) {
+  if (!Users_ID || !Users_Email || !Users_Username || !UsersType_ID || !Users_Type) {
     return 0;
   } else {
-    if (Login_Type === 'website' || Login_Type === 'application') {
-      const Token = jwt.sign(
+    const Token = jwt.sign(
       {
         Users_ID: Users_ID,
         Users_Email: Users_Email,
         Users_Username: Users_Username,
         UsersType_ID: UsersType_ID,
-        Users_Type: Users_Type,
-        Login_Type: Login_Type
+        Users_Type: Users_Type
       },
-      process.env.PRIVATE_TOKEN_KEY, 
+      process.env.PRIVATE_TOKEN_KEY,
       { expiresIn: '24h' }
     );
     return Token;
-    }else {
-      return 0;
-    }
+
   }
 }
 
--- server.js
+++ server.js
@@ -169,12 +169,108 @@ app.post('/api/verifyToken', RateLimiter(0.5 * 60 * 1000, 15), VerifyTokens, (re
       Users_Username: userData.Users_Username,
       UsersType_ID: userData.UsersType_ID,
       Users_Type: userData.Users_Type,
-      Login_Type: userData.Login_Type,
       message: 'Token is valid.',
       status: true,
     });
   }
   return res.status(402).json({ message: 'Invalid Token.', status: false });
+});
+
+////////////////////////////////// Authentication API ///////////////////////////////////////
+// API Login Website
+app.post('/api/login', RateLimiter(1 * 60 * 1000, 5), async (req, res) => {
+  let { Users_Email, Users_Password } = req.body || {};
+
+  if (!Users_Email || !Users_Password ||
+    typeof Users_Email !== 'string' || typeof Users_Password !== 'string') {
+    return res.status(400).json({ message: 'Please fill in the correct parameters as required.', status: false });
+  }
+
+  Users_Email = xss(validator.escape(Users_Email));
+  Users_Password = xss(Users_Password);
+
+  try {
+    const sql = `SELECT Users_ID, Users_Email, Users_Username, Users_Password, Users_Type
+      FROM users WHERE (Users_Username = ? OR Users_Email = ?) AND (Users_Type = 'patient' OR Users_Type = 'doctor') AND Users_IsActive = 1`;
+
+    db.query(sql, [Users_Email, Users_Email], async (err, result) => {
+      if (err) {
+        console.error('Database error (users)', err);
+        return res.status(500).json({ message: 'An error occurred on the server.', status: false });
+      }
+
+      if (result.length === 0) {
+        return res.status(401).json({ message: "Email or password is incorrect.", status: false });
+      }
+
+      const user = result[0];
+      const passwordMatch = await bcrypt.compare(Users_Password, user.Users_Password);
+      if (!passwordMatch) {
+        return res.status(401).json({ message: "Email or password is incorrect.", status: false });
+      }
+
+      // Check Users_Type
+      let sql_users_type, users_type_name_id;
+      if (user.Users_Type === 'patient') {
+        sql_users_type = "SELECT Patient_ID FROM patient WHERE Users_ID = ?";
+        users_type_name_id = 'Patient_ID';
+      } else if (user.Users_Type === 'doctor') {
+        sql_users_type = "SELECT Doctor_ID FROM doctor WHERE Users_ID = ?";
+        users_type_name_id = 'Doctor_ID';
+      } else {
+        return res.status(400).json({ message: 'Invalid user type.', status: false });
+      }
+
+      db.query(sql_users_type, [user.Users_ID], async (err, result_users_type) => {
+        if (err) {
+          console.error('Database error (user type)', err);
+          return res.status(500).json({ message: 'An error occurred on the server.', status: false });
+        }
+
+        if (result_users_type.length === 0) {
+          return res.status(404).json({ message: 'User type details not found.', status: false });
+        }
+
+        const userType = result_users_type[0];
+        const UsersType_ID = userType[users_type_name_id];
+
+        const token = GenerateTokens(
+          user.Users_ID,
+          user.Users_Email,
+          user.Users_Username,
+          UsersType_ID,
+          user.Users_Type
+        );
+
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: isProduction,
+          sameSite: isProduction ? "None" : "Lax",
+          domain: isProduction ? process.env.COOKIE_DOMAIN_PROD : undefined,
+          maxAge: 60 * 60 * 1000
+        });
+
+        res.status(200).json({
+          message: "The login was successful.",
+          status: true
+        });
+      });
+    });
+  } catch (error) {
+    console.error('Catch error', error);
+    res.status(500).json({ message: 'An unexpected error occurred.', status: false });
+  }
+});
+
+// API Logout Website
+app.post('/api/logout', (req, res) => {
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? 'None' : 'Lax',
+    domain: isProduction ? process.env.COOKIE_DOMAIN_PROD : undefined
+  });
+  res.status(200).json({ message: 'Logged out successfully.', status: true });
 });
 
 /////////////////////////////////////////////////////////////////////////////////////
